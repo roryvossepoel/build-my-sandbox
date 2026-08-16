0e2728d90117c1427887adf4588cb8b699cbfc7f
Rory Vossepoel <roryvossepoel@gmail.com>
2026-08-16 23:54:35 +0200

    Add initial WSB generator

---
+++ src/generator/wsb.ts
@@ -0,0 +1,29 @@
+import type { SandboxProfile } from '../types.js';
+
+const boolSetting = (value: boolean | undefined, enabled: string, disabled: string) =>
+  value === false ? disabled : enabled;
+
+const escapeXml = (value: string) =>
+  value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+
+export function generateWsb(profile: SandboxProfile): string {
+  const memory = profile.sandbox.memoryMB ?? 4096;
+  const networking = boolSetting(profile.sandbox.networking, 'Enable', 'Disable');
+  const clipboard = boolSetting(profile.sandbox.clipboard, 'Enable', 'Disable');
+  const vGpu = boolSetting(profile.sandbox.vGpu, 'Enable', 'Disable');
+
+  const mappedFolders = (profile.mappedFolders ?? []).length
+    ? `\n  <MappedFolders>\n${(profile.mappedFolders ?? [])
+        .map(
+          (folder) => `    <MappedFolder>\n      <HostFolder>${escapeXml(folder.hostFolder)}</HostFolder>\n      <SandboxFolder>${escapeXml(folder.sandboxFolder)}</SandboxFolder>\n      <ReadOnly>${folder.readOnly ? 'true' : 'false'}</ReadOnly>\n    </MappedFolder>`,
+        )
+        .join('\n')}\n  </MappedFolders>`
+    : '';
+
+  return `<Configuration>\n  <MemoryInMB>${memory}</MemoryInMB>\n  <Networking>${networking}</Networking>\n  <ClipboardRedirection>${clipboard}</ClipboardRedirection>\n  <VGpu>${vGpu}</VGpu>${mappedFolders}\n  <LogonCommand>\n    <Command>powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File C:\\SandboxBuilder\\runner.ps1</Command>\n  </LogonCommand>\n</Configuration>\n`;
+}
