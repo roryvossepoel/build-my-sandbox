d763dd698815f26148dec185b9861a84190b8f05
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-01 15:25:23 -0700

    Use favicon mark in header

---
+++ src/brand-shell.ts
@@ -0,0 +1,12 @@
+function unifyBrandIcon() {
+  const headerMark = document.querySelector<HTMLElement>('.playground-topbar .sand-logo');
+  if (!headerMark || headerMark.querySelector('img')) return;
+  headerMark.innerHTML = '<img src="./favicon.svg" alt="" aria-hidden="true" />';
+}
+
+unifyBrandIcon();
+
+const brandRoot = document.querySelector('#app');
+if (brandRoot) {
+  new MutationObserver(() => unifyBrandIcon()).observe(brandRoot, { childList: true, subtree: true });
+}
