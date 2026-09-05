576288ba6b5db0fffb027585f4bd64f8c4426393
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-06 00:57:31 +0200

    Include FAQ and Learn pages in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        faq: 'faq.html',
+        learn: 'learn.html',
+      },
+    },
+  },
 });
